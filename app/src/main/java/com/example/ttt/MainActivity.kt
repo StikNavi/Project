f60268f312cf32d4ce76705a2226d395764fa58a
StikNavi <stiknavi@gmail.com>
2024-02-17 12:41:01 +0200

    Опис вашого коміту тут

--- app/src/main/java/com/example/ttt/MainActivity.kt
+++ app/src/main/java/com/example/ttt/MainActivity.kt
@@ -3,13 +3,18 @@ package com.example.ttt
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.ttt.ui.theme.TttTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,12 +22,11 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             TttTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    ImageSlider()
                 }
             }
         }
@@ -30,17 +34,45 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun ImageSlider() {
+    var currentImageIndex by remember { mutableStateOf(0) }
+
+    val images = listOf<String>(
+        "Image 1",
+        "Image 2",
+        "Image 3"
     )
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = images[currentImageIndex],
+            modifier = Modifier.padding(16.dp)
+        )
+
+        Button(
+            onClick = { currentImageIndex = (currentImageIndex - 1 + images.size) % images.size },
+            modifier = Modifier.padding(8.dp)
+        ) {
+            Text("Попереднє зображення")
+        }
+
+        Button(
+            onClick = { currentImageIndex = (currentImageIndex + 1) % images.size },
+            modifier = Modifier.padding(8.dp)
+        ) {
+            Text("Наступне зображення")
+        }
+    }
 }
+
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun ImageSliderPreview() {
     TttTheme {
-        Greeting("Android")
+        ImageSlider()
     }
 }
