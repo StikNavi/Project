ea26785d5ba43b5e064d4cb3933198803d73830a
StikNavi <stiknavi@gmail.com>
2024-02-17 13:04:23 +0200

    changes 2

--- app/src/main/java/com/example/ttt/MainActivity.kt
+++ app/src/main/java/com/example/ttt/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.ttt
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -13,6 +14,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.ttt.ui.theme.TttTheme
@@ -37,30 +39,36 @@ class MainActivity : ComponentActivity() {
 fun ImageSlider() {
     var currentImageIndex by remember { mutableStateOf(0) }
 
-    val images = listOf<String>(
-        "Image 1",
-        "Image 2",
-        "Image 3"
+    val imagesWithText = listOf(
+        Pair(R.drawable.test, "Текст для зображення 1"),
+        Pair(R.drawable.test, "Текст для зображення 2"),
+        Pair(R.drawable.test, "Текст для зображення 3")
     )
 
     Column(
         modifier = Modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        Image(
+            painter = painterResource(id = imagesWithText[currentImageIndex].first),
+            contentDescription = null,
+            modifier = Modifier.padding(16.dp)
+        )
+
         Text(
-            text = images[currentImageIndex],
+            text = imagesWithText[currentImageIndex].second,
             modifier = Modifier.padding(16.dp)
         )
 
         Button(
-            onClick = { currentImageIndex = (currentImageIndex - 1 + images.size) % images.size },
+            onClick = { currentImageIndex = (currentImageIndex - 1 + imagesWithText.size) % imagesWithText.size },
             modifier = Modifier.padding(8.dp)
         ) {
             Text("Попереднє зображення")
         }
 
         Button(
-            onClick = { currentImageIndex = (currentImageIndex + 1) % images.size },
+            onClick = { currentImageIndex = (currentImageIndex + 1) % imagesWithText.size },
             modifier = Modifier.padding(8.dp)
         ) {
             Text("Наступне зображення")
